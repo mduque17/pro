6b80766e2f4310c9c3637cbd45831ba9452b7e69
mduque17 <70531651+mduque17@users.noreply.github.com>
2022-03-07 11:00:49 -0500

    Add files via upload

---
+++ act21feb.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Actividad</title>
+</head>
+<body>
+<style>
+	.a{
+		position: absolute;
+		top: 5%;
+		left: 5%;
+	}
+	.b{
+		position: absolute;
+		top: 5%;
+		left: 15%;
+	}
+</style>
+<div class="a">
+	<input type="" name="" placeholder="x1">
+	<input type="" name="" placeholder="y1">
+</div>
+
+<div class="b">
+	<input type="" name="" placeholder="x2">
+	<input type="" name="" placeholder="y2">
+</div>
+
+<div></div>
+<div></div>
+</body>
+</html>
